bb50562dee50ef7e75c47369bd27ffc5240a4d6a
MK <mk@users.noreply.github.com>
2025-06-23 11:31:48 +0300

    fix: Serve media files correctly when DEBUG is False

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 """
 URL configuration for core project.
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,7 +12,8 @@ urlpatterns = [
 ]
 
 # Настраиваем раздачу медиа-файлов (загруженных изображений)
-# через Django-сервер. В production-среде этим должен заниматься
-# веб-сервер (например, Nginx), но для данного проекта
-# раздача через Django является допустимым упрощением.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# через Django-сервер.
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]

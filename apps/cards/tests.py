d6624efb5e34ed120a3aa3bc05361afc69b78413
Maksim <43213750+Waksim@users.noreply.github.com>
2025-11-15 16:17:34 +0300

    Normalize Wanderer name during card sync

--- apps/cards/tests.py
+++ apps/cards/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.cards.services.db_updater import _normalize_card_name
+
+
+class NormalizeCardNameTests(TestCase):
+    def test_wanderer_name_normalized_by_id(self):
+        self.assertEqual(_normalize_card_name(1506, "Anything"), "Wanderer")
+
+    def test_wanderer_name_normalized_by_value(self):
+        template_name = "#{REALNAME[ID(1)|DELAYHANDLE(true)]}"
+        self.assertEqual(_normalize_card_name(9999, template_name), "Wanderer")
+
+    def test_regular_name_remains_unchanged(self):
+        self.assertEqual(_normalize_card_name(1, "Diluc"), "Diluc")
